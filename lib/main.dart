311993d24d96043d55d0972ba3964a6ea784dd48
Teerit Ploensin <teeritploensin@gmail.com>
2021-10-19 20:23:01 +0700

    Added starter application

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/teerit.jpg'),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.teal[200],
+    );
+  }
+}
