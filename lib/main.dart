6edfde9ddf7b84cd399f31f104afb80b3aeb15d5
Teerit Ploensin <teeritploensin@gmail.com>
2021-10-19 20:46:07 +0700

    Added some text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,28 @@ class Home extends StatelessWidget {
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/teerit.jpg'),
+          children: [
+            const CircleAvatar(
+                radius: 50, backgroundImage: AssetImage('images/teerit.jpg')),
+            Text('Teerit Ploensin',
+                style: GoogleFonts.pacifico(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Text(
+              'Software Engineer',
+              style: GoogleFonts.sourceSansPro(
+                  fontSize: 30,
+                  color: Colors.teal[50],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color: Colors.teal.shade700,
+              ),
             ),
           ],
         ),
